e577969d1cb1a34b686db2867ed6adff2fda1bf4
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-01-05 19:54:45 -0600

    Added: SkillSettings Provide available skill list.

---
+++ Runtime/SkillSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Hextant;
+
+#if UNITY_EDITOR
+using Hextant.Editor;
+using UnityEditor;
+#endif
+
+namespace FinTOKMAK.SkillSystem.RunTime
+{
+    [Settings( SettingsUsage.RuntimeProject, "FinTOKMAK Skill System" )]
+    public class SkillSettings : Settings<SkillSettings>
+    {
+        public List<Skill> availableSkills;
+        
+#if UNITY_EDITOR
+        [SettingsProvider]
+        static SettingsProvider GetSettingsProvider() =>
+            instance.GetSettingsProvider();
+#endif
+    }
+}
